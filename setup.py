961055794ad08edaf351c86af0a72d914454b6d3
Jonathan Karr <jonrkarr@gmail.com>
2016-10-26 02:34:30 -0400

    using dependency links

--- setup.py
+++ setup.py
@@ -7,16 +7,29 @@ import sys
 
 # parse requirements.txt
 install_requires = []
+tests_require = []
+dependency_links = []
+
 for line in open('requirements.txt'):
     pkg_src = line.rstrip()
     match = re.match('^.+#egg=(.*?)$', pkg_src)
     if match:
         pkg_id = match.group(1)
-        pip.main(['install', '-U', pkg_src])
+        dependency_links.append(pkg_src)
     else:
         pkg_id = pkg_src
     install_requires.append(pkg_id)
-    
+
+for line in open('tests/requirements.txt'):
+    pkg_src = line.rstrip()
+    match = re.match('^.+#egg=(.*?)$', pkg_src)
+    if match:
+        pkg_id = match.group(1)
+        dependency_links.append(pkg_src)
+    else:
+        pkg_id = pkg_src
+    tests_require.append(pkg_id)
+
 setup(
     name="Karr-Lab-build-utils",
     version=karr_lab_build_utils.__version__,
@@ -32,6 +45,8 @@ setup(
         'karr_lab_build_utils': ['lib'],
     },
     install_requires=install_requires,
+    tests_require=tests_require,
+    dependency_links=dependency_links,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
