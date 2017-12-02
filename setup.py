fe259d7207809f8ac6030d3a6ae1bf8686110892
Jonathan Karr <jonrkarr@gmail.com>
2017-12-02 14:59:48 -0500

    making pygit2 optional, switching to RTD conda

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 import os
 import pip
 import re
+pip.main(['install', 'git+https://github.com/davidfischer/requirements-parser.git'])
+import requirements
 import sys
 
 # get long description
@@ -16,30 +18,66 @@ with open('karr_lab_build_utils/VERSION', 'r') as file:
     version = file.read().strip()
 
 # parse dependencies and their links from requirements.txt files
-install_requires = []
-tests_require = []
-dependency_links = []
+def parse_requirements(filename, install_requires, extras_require, dependency_links):
+    if os.path.isfile(filename):
+        with open(filename, 'r') as file:
+            for req in requirements.parse(file):
+                line = req.line
+                if '#egg=' in line:
+                    if line.find('#') < line.find('#egg='):
+                        line = line[0:line.find('#')]
+                    else:
+                        line = line[0:line.find('#', line.find('#egg=')+5)]
+                else:
+                    if '#' in line:
+                        line = line[0:line.find('#')]
+                if ';' in line:
+                    marker = line[line.find(';')+1:].strip()
+                    marker_match = pkg_resources.Requirement.parse(req.name + '; ' + marker).marker.evaluate()
 
-for line in open('requirements.txt'):
-    pkg_src = line.rstrip()
-    match = re.match('^.+#egg=(.*?)$', pkg_src)
-    if match:
-        pkg_id = match.group(1)
-        dependency_links.append(pkg_src)
-    else:
-        pkg_id = pkg_src
-    install_requires.append(pkg_id)
+                else:
+                    marker = ''
+                    marker_match = True
 
-for line in open('tests/requirements.txt'):
-    pkg_src = line.rstrip()
-    match = re.match('^.+#egg=(.*?)$', pkg_src)
-    if match:
-        pkg_id = match.group(1)
-        dependency_links.append(pkg_src)
-    else:
-        pkg_id = pkg_src
-    tests_require.append(pkg_id)
-dependency_links = list(set(dependency_links))
+                req_setup = req.name + ','.join([''.join(spec) for spec in req.specs]) + ('; ' if marker else '') + marker
+
+                if req.extras:
+                    for option in req.extras:
+                        if option not in extras_require:
+                            extras_require[option] = set()
+                        extras_require[option].add(req_setup)
+                else:
+                    install_requires.add(req_setup)
+
+                if req.uri:
+                    if req.revision:
+                        dependency_links[marker_match].add(req.uri + '@' + req.revision)
+                    else:
+                        dependency_links[marker_match].add(req.uri)
+
+install_requires = set()
+tests_require = set()
+docs_require = set()
+extras_require = {}
+dependency_links = {True: set(), False: set()}
+
+parse_requirements('requirements.txt', install_requires, extras_require, dependency_links)
+parse_requirements('tests/requirements.txt', tests_require, extras_require, dependency_links)
+parse_requirements('docs/requirements.txt', docs_require, extras_require, dependency_links)
+
+tests_require = tests_require.difference(install_requires)
+docs_require = docs_require.difference(install_requires)
+
+extras_require['tests'] = tests_require
+extras_require['docs'] = docs_require
+
+install_requires = list(install_requires)
+tests_require = list(tests_require)
+docs_require = list(docs_require)
+for option, reqs in extras_require.items():
+    extras_require[option] = list(reqs)
+for marker_match, reqs in dependency_links.items():
+    dependency_links[marker_match] = list(reqs)
 
 # install non-PyPI dependencies because setup doesn't do this correctly
 for dependency_link in dependency_links:
@@ -75,6 +113,7 @@ setup(
         ],
     },
     install_requires=install_requires,
+    extras_require=extras_require,
     tests_require=tests_require,
     dependency_links=dependency_links,
     classifiers=[
