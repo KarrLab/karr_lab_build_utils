fa1b6bc013a9eff296e9e026ea8f8d1e1ec4e8bc
Jonathan Karr <jonrkarr@gmail.com>
2018-08-08 16:28:59 -0400

    updating to latest docker image

--- karr_lab_build_utils/core.py
+++ karr_lab_build_utils/core.py
@@ -157,7 +157,7 @@ class BuildHelper(object):
     """
 
     INITIAL_PACKAGE_VERSION = '0.0.1'
-    DEFAULT_BUILD_IMAGE_VERSION = '0.0.24'
+    DEFAULT_BUILD_IMAGE_VERSION = 'latest'
 
     DEFAULT_TEST_RUNNER = 'pytest'
     DEFAULT_PROJ_TESTS_DIR = 'tests'
